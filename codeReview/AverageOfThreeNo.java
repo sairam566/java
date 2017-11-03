78199ac9ce253db22f3d0288697f13ca0325cf15
sairam <sairam1431995@gmail.com>
2017-11-03 19:14:22 +0530

    Add files via upload

---
+++ codeReview/AverageOfThreeNo.java
@@ -0,0 +1,20 @@
+package assigmnent1;
+
+public class AverageOfThreeNo
+{
+	public static void main(String[] args)
+	{
+		int value1=10;
+		int value2=20;
+		int value3=30;
+		
+		int total,average;
+		
+		total=value1+value2+value3;
+		average=total/3;
+		
+		System.out.println("The Total of "+value1+" + "+value2+" + "+value3+"  is :"+total);
+		System.out.println("The Average of "+value1+" , "+value2+" , "+value3+" is :"+average);
+	}
+
+}
