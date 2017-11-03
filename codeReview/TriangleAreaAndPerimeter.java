c4d4ee1b3d39d1a578dcf9df1624334983b4a5d0
sairam <sairam1431995@gmail.com>
2017-11-03 19:03:11 +0530

    Add files via upload

---
+++ codeReview/TriangleAreaAndPerimeter.java
@@ -0,0 +1,21 @@
+package assigmnent1;
+
+public class TriangleAreaAndPerimeter
+{
+	public static void main(String[] args)
+	{	
+		int radious=2;
+		float PI=3.14f;
+		
+		float area,perimeter;
+		
+		area=PI*radious*radious;
+		
+		perimeter=2*PI*radious;
+		
+		System.out.println("The Area of a Circle with Radious "+radious+" is :"+area);
+		
+		System.out.println("The Perimeter of a Circle with Radious "+radious+" is :"+perimeter);
+	}
+
+}
