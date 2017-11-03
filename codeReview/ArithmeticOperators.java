765022e578278dbfe106064bc4388f74962955f4
sairam <sairam1431995@gmail.com>
2017-11-03 18:53:07 +0530

    Add files via upload

---
+++ codeReview/ArithmeticOperators.java
@@ -0,0 +1,25 @@
+package assigmnent1;
+
+public class ArithmeticOperators
+{
+	public static void main(String[] args)
+	{	
+		int value1 = 10;
+		int value2 = 20;
+		
+		int addition , subraction , division , multiplication;
+		
+		addition = value1 + value2;
+		
+		subraction = value1 - value2;
+		
+		division = value1 / value2;
+		
+		multiplication = value1 * value2;
+		
+		System.out.println("The Addition of       "+value1+" + "+value2+" = "+addition);
+		System.out.println("The Subraction of     "+value1+" - "+value2+" = "+subraction);
+		System.out.println("The Division of       "+value1+" / "+value2+" = "+division);
+		System.out.println("The Multiplication of "+value1+" * "+value2+" = "+multiplication);
+	}
+}
