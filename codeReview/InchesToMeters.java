317f2682351ef1d09c6c53bbf1b3d3d9b6038521
sairam <sairam1431995@gmail.com>
2017-11-04 10:28:20 +0530

    Add files via upload

---
+++ codeReview/InchesToMeters.java
@@ -0,0 +1,16 @@
+package assignment2;
+
+public class InchesToMeters
+{
+	public static void main(String[] args) 
+	{
+		float inches=12;
+		
+		float meters;
+		
+		meters=inches*0.0254f;
+		
+		System.out.println(inches+" Inches is EqualTo "+meters);
+	}
+
+}
