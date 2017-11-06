edde7acd33a3cffbd597e5811ff3fa1890353075
sairam <sairam1431995@gmail.com>
2017-11-06 10:26:21 +0530

    Add files via upload

---
+++ codeReview/GreatesOfThreeNo.java
@@ -0,0 +1,23 @@
+package assignment3;
+
+public class GreatesOfThreeNo
+{
+	public static void main(String[] args)
+	{
+		int value1=80;
+		int value2=30;
+		int value3=25;
+		if(value1>value2)
+		{
+			System.out.println("Value "+value1+" is Big");
+		}
+	     else if(value2>value3)
+		{
+			System.out.println("Value "+value2+" is Big");
+		}
+		else
+		{
+			System.out.println("Value "+value3+" is Big");
+		}
+	}
+}
