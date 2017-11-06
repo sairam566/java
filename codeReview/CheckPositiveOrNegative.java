78aed2675657354af9d341d8b24d3137813bcdf2
sairam <sairam1431995@gmail.com>
2017-11-06 10:45:08 +0530

    Add files via upload

---
+++ codeReview/CheckPositiveOrNegative.java
@@ -0,0 +1,17 @@
+package assignment3;
+
+public class CheckPositiveOrNegative 
+{
+	public static void main(String[] args)
+	{
+		int value=0;
+		if(value<0)
+		{
+			System.out.println("The Number is Negative "+value);
+		}
+		else
+		{
+		System.out.println("The number is Positive "+value);
+		}
+	}
+}
