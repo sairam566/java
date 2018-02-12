b6bd0c16f5269f54c5cbc3c0a02b7b83e1ee2e57
sairam <sairam1431995@gmail.com>
2018-02-12 16:37:40 +0530

    Update ArrayList_indexOf_Demo.java

--- Collection/ArrayList_Each_Method_Example/ArrayList_indexOf_Demo.java
+++ Collection/ArrayList_Each_Method_Example/ArrayList_indexOf_Demo.java
@@ -5,18 +5,24 @@ public class ArrayList_indexOf_Demo {
 	public static void main(String[] args) {
 		ArrayList<Integer> arrayList=new ArrayList<Integer>();
 		arrayList.add(10);
-		arrayList.add(20);
+		arrayList.add(10);
 		arrayList.add(30);
 		arrayList.add(40);
 		
-		//This method is used to return the index position of an Object. 
+		//This method is used to return the First occurance of index position of an Object. 
 		
-		System.out.println("The index position of 30 is: "+arrayList.indexOf(30));
+		System.out.println("The index position of 10 is: "+arrayList.indexOf(10));
+		
+		
+		//This method return -1 if the Object is not present in the arrayList.
+		System.out.println("The index position of 50 is: "+arrayList.indexOf(50));
 	}
 }
 /*
  * OutPut
  * ----------------------------------
- * The index position of 30 is: 2
+ * The index position of 10 is:0
+ *
+ *The index position of 50 is: -1
  * */
  
