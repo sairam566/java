7fdfac8bee9f5104f3ba0596d14a78086416332c
sairam <sairam1431995@gmail.com>
2018-04-03 16:51:51 +0530

    Create Answer.java

---
+++ Springs/3-Apr-2018_Constructor Injection with Non-String Collection/Answer.java
@@ -0,0 +1,20 @@
+
+public class Answer {
+
+	private int id;
+	private String name;
+	private String by;
+	
+	public Answer(int id, String name, String by) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.by = by;
+	}
+	
+	public String toString()
+	{
+		return "Comment Id "+id+"\n Comment By "+name+"\n Comment is "+by;
+	}
+	
+}
